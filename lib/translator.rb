eb64e493a6c81f0abe72cc625b3f3f0a5d3924d6
Don Namgyal <dhondup.namgyal@gmail.com>
2020-04-09 18:13:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,14 @@
 # require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+def load_library(path)
+  lib = YAML.load_file(path)
 end
 
 def get_japanese_emoticon
   # code goes here
+  
 end
 
 def get_english_meaning
