c0f3a7410d673980f6b9127173a0c5e1fc20b315
Don Namgyal <dhondup.namgyal@gmail.com>
2020-04-09 18:13:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,11 +4,16 @@ require "pry"
 
 def load_library(path)
   lib = YAML.load_file(path)
+  new_data = {}
+  lib.each do |key, value|
+    new_data[:name] = key
+  end
+  binding.pry
 end
 
 def get_japanese_emoticon
   # code goes here
-  
+
 end
 
 def get_english_meaning
